716116830132f63bc68906d32ea3fcf7f19bf3a5
RickL <511189918@qq.com>
2019-03-14 11:35:10 +0800

    fix ios bug

--- js/panorama.js
+++ js/panorama.js
@@ -16,7 +16,7 @@ let theta = 0
 let texture = null
 let	target = new THREE.Vector3()
 
-let	video = null
+let	m_video = null
 let video_duration = 0
 let zoom_control = null
 let m_play_btn = null
@@ -28,14 +28,21 @@ let touch_mask = null
 let progress_timer = null
 let is_video_ready = false
 
-let ZOOM = 1
+let ZOOM = 0.5
 
-initVideoControl()
+initVideoControl()	
 initPanorama()
 animate()
 
-function initVideoControl() {
-	video = document.getElementById('video')
+
+function init(src) {
+	initVideoControl(src)	
+	initPanorama()
+	animate()
+}
+
+function initVideoControl(src) {
+	m_video = document.getElementById('video')
 	touch_mask = document.getElementById('touch-mask')
 	zoom_control = document.getElementsByClassName('zoom-control')[0]
 	m_play_btn = document.getElementsByClassName('play-btn-x')[0]
@@ -43,10 +50,13 @@ function initVideoControl() {
 	progress = document.getElementsByClassName('progress')[0]
 	time_state = document.getElementsByClassName('time')[0]
 
-	video.addEventListener('loadedmetadata', onVideoLoadedMetaData)
-	video.addEventListener('play', onVideoPlay)
-	video.addEventListener('pause', onVideoPause)
-	video.addEventListener('ended', onVideoEnd)
+	// m_video.src = "http://stdag-image.oss-cn-beijing.aliyuncs.com/em1_video/20190308/313847173936313221002e00_1551888000-20190308113432595.mp4"
+	// m_video.src = src
+
+	m_video.addEventListener('loadedmetadata', onVideoLoadedMetaData)
+	m_video.addEventListener('play', onVideoPlay)
+	m_video.addEventListener('pause', onVideoPause)
+	m_video.addEventListener('ended', onVideoEnd)
 
 	zoom_control.addEventListener('click', onZoomBtnPress, true)
 	m_play_btn.addEventListener('click', onMainPlayBtnPress, true)
@@ -54,68 +64,59 @@ function initVideoControl() {
 }
 
 function initPanorama() {
-	var container = document.getElementById('container');
+	var container = document.getElementById('container')
 	// 初始化相机
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100);
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100)
+	camera.zoom = ZOOM
+	camera.updateProjectionMatrix()
 	// 初始化场景
-	scene = new THREE.Scene();
-	// 创建贴图占位
-	texture_placeholder = document.createElement('canvas');
-	texture_placeholder.width = 128;
-	texture_placeholder.height = 128;
+	scene = new THREE.Scene()
 
-	var context = texture_placeholder.getContext('2d');
-	context.fillStyle = 'rgb( 200, 200, 200 )';
-	context.fillRect(0, 0, texture_placeholder.width, texture_placeholder.height);
+	// 初始化渲染器
+	renderer = new THREE.WebGLRenderer()
+	renderer.setPixelRatio(window.devicePixelRatio)
+	renderer.setSize(window.innerWidth, window.innerHeight)
+	container.appendChild(renderer.domElement)
+	renderer.autoClear = false
 
-	// 贴图
 
-	texture = new THREE.VideoTexture(video);
-	texture.minFilter = THREE.LinearFilter;
-	texture.magFilter = THREE.LinearFilter;
-	texture.format = THREE.RGBFormat;
-	var material =  new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 })
+	texture = new THREE.VideoTexture(m_video)
+	texture.minFilter = THREE.LinearFilter
+	texture.magFilter = THREE.LinearFilter
+	texture.format = THREE.RGBFormat
+	let material =  new THREE.MeshBasicMaterial({ map: texture, overdraw: 0.5 })
 	// 创建几何体
-	var geometry = new THREE.SphereGeometry(500, 60, 40);
-	geometry.scale(- 1, 1, 1);
+	let geometry = new THREE.SphereGeometry(500, 60, 40)
+	geometry.scale(-1, 1, 1)
 	// 贴图
-	var mesh = new THREE.Mesh(geometry, material)
+	let mesh = new THREE.Mesh(geometry, material)
 	// 添加到场景
 	scene.add(mesh)
-	// 初始化渲染器
-	renderer = new THREE.WebGLRenderer();
-	renderer.setPixelRatio(window.devicePixelRatio);
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	// renderer.setSize(270, 135);
-	container.appendChild(renderer.domElement);
-	renderer.autoClear = false;
+
 
 	// 监听触摸事件
-	touch_mask.addEventListener('touchstart', onDocumentTouchStart, false);
-	touch_mask.addEventListener('touchmove', onDocumentTouchMove, false);
-	touch_mask.addEventListener('touchend', onDocumentTouchEnd, false);
-	window.addEventListener('resize', onWindowResize, false);
-
-	
-
+	touch_mask.addEventListener('touchstart', onDocumentTouchStart, false)
+	touch_mask.addEventListener('touchmove', onDocumentTouchMove, false)
+	touch_mask.addEventListener('touchend', onDocumentTouchEnd, false)
+	window.addEventListener('resize', onWindowResize, false)
 }
 
 // 监听窗口大小调整事件
 function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 // 监听触摸开始事件
 function onDocumentTouchStart(event) {
-	isUserInteracting = true;
+	isUserInteracting = true
 	if (event.touches.length == 1) {
 		// event.preventDefault();
-		onPointerDownPointerX = event.touches[0].pageX;
-		onPointerDownPointerY = event.touches[0].pageY;
-		onPointerDownLon = lon;
-		onPointerDownLat = lat;
+		onMouseDownMouseX = event.touches[0].pageX
+		onMouseDownMouseY = event.touches[0].pageY
+		onMouseDownLon = lon
+		onMouseDownLat = lat
 	}
 }
 
@@ -123,35 +124,35 @@ function onDocumentTouchStart(event) {
 function onDocumentTouchMove(event) {
 	if (event.touches.length == 1) {
 		// event.preventDefault();
-		lon = (onPointerDownPointerX - event.touches[0].pageX) * 0.1 + onPointerDownLon;
-		lat = (event.touches[0].pageY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
+		lon = (onMouseDownMouseX - event.touches[0].pageX) * 0.1 + onMouseDownLon
+		lat = (event.touches[0].pageY - onMouseDownMouseY) * 0.1 + onMouseDownLat
 	}
 }
 
 // 监听触摸滑动事件
-function onDocumentTouchEnd(event) {
-	isUserInteracting = false;
+function onDocumentTouchEnd() {
+	isUserInteracting = false
 }
 
 // 监听视频获取基础信息事件
 function onVideoLoadedMetaData() {
 	is_video_ready = true
-	video_duration = video.duration
+	video_duration = m_video.duration
 	time_state.textContent = '00:00/' + getForamtTimeStr(video_duration)
 }
 
 // 监听视频播放事件
 function onVideoPlay() {
 	progress_timer && clearInterval(progress_timer)
-	progress_timer = setInterval(function(){
-		time_state.textContent = getForamtTimeStr(video.currentTime) + '/' + getForamtTimeStr(video_duration)
-		progress.value = (video.currentTime / video_duration) * 100
+	progress_timer = setInterval(function() {
+		time_state.textContent = getForamtTimeStr(m_video.currentTime) + '/' + getForamtTimeStr(video_duration)
+		progress.value = (m_video.currentTime / video_duration) * 100
 	}, 1000)
 }
 
 // 监听视频暂停事件
 function onVideoPause() {
-	progress.value = video.currentTime / video_duration
+	progress.value = m_video.currentTime / video_duration
 	progress_timer && clearInterval(progress_timer)
 }
 
@@ -163,9 +164,9 @@ function onVideoEnd() {
 // 监听播放按钮点击事件
 function onMainPlayBtnPress() {
 	if (!is_video_ready) return
-	m_play_btn.style = 'display:none;'
+	m_play_btn.className = 'play-btn-x hide'
 	control_btn.className = 'control-btn pause'
-	video.play()
+	m_video.play()
 }
 
 // 监听控制栏按钮点击事件
@@ -173,14 +174,14 @@ function onControlBtnPress() {
 	if (!is_video_ready) return
 	let state = control_btn.className.split(' ')[1]
 	if (state == 'play') {
-		m_play_btn.style = 'display:none;'
+		m_play_btn.className = 'play-btn-x hide'
 		control_btn.className = 'control-btn pause'
-		video.play()
+		m_video.play()
 	}
 	if (state == 'pause') {
-		m_play_btn.style = ''
+		m_play_btn.className = 'play-btn-x'
 		control_btn.className = 'control-btn play'
-		video.pause()
+		m_video.pause()
 	}
 }
 
@@ -213,24 +214,25 @@ function getForamtTimeStr(seconds) {
 
 // 绘制循环
 function animate() {
-	requestAnimationFrame(animate);
-	update();
+	requestAnimationFrame(animate)
+	update()
 }
 
 // 刷新函数
 function update() {
 	if (isUserInteracting === false && autoAnimate === true) {
-		lon += 0.1;
+		lon += 0.1
 	}
-	lat = Math.max(- 85, Math.min(85, lat));
-	phi = THREE.Math.degToRad(90 - lat);
-	theta = THREE.Math.degToRad(lon);
+	lat = Math.max(-85, Math.min(85, lat))
+	phi = THREE.Math.degToRad(90 - lat)
+	theta = THREE.Math.degToRad(lon)
 
-	target.x = 500 * Math.sin(phi) * Math.cos(theta);
-	target.y = 500 * Math.cos(phi);
-	target.z = 500 * Math.sin(phi) * Math.sin(theta);
+	target.x = 500 * Math.sin(phi) * Math.cos(theta)
+	target.y = 500 * Math.cos(phi)
+	target.z = 500 * Math.sin(phi) * Math.sin(theta)
 
-	camera.lookAt(target);
+	camera.lookAt(target)
 
-	renderer.render(scene, camera);
+	renderer.render(scene, camera)
 }
+
